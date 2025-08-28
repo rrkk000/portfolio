131630391f5d230ca6c26e08d1a28d80c1af0106
кристина <krk188m@gmail.com>
2025-08-28 16:43:14 +0300

    готово

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import CursorGlow from "./components/CursorGlow/CursorGlow";
 import Header from "./components/Header/Header";
